931f8fb6f283ac2adf23cf8480437205712498f8
Tahmid Zaman <tahmid198@yahoo.com>
2021-12-21 01:56:55 -0500

    added functionality to purchase tokens from token sale
    sell event created, sold tokens are tracked, tokens transfer upon purchase, token sale contract has tokens for sale

--- contracts/MyTokenSale.sol
+++ contracts/MyTokenSale.sol
@@ -3,18 +3,64 @@ pragma solidity ^0.5.16; // version of solidity being used
 import "./MyToken.sol";
 
 contract MyTokenSale {
-	address admin; // admin does not have public visiblity becasue we do not want to expose admin address
+	address payable admin; // admin does not have public visiblity becasue we do not want to expose admin address
 	MyToken public tokenContract; // solidity will give a tokenContract() function for free because variable is made public
-	uint256 public tokenPrice; 
+	uint256 public tokenPrice;
+	uint256 public tokensSold; // keep track of number of token sold
+
+	event sell(address _buyer, uint256 _amount); // sell event
 
 	constructor (MyToken _tokenContract, uint256 _tokenPrice) public {
-		// assign an admin (an external account connected to the blockchain that has special privleges that other accounts do not have, such as ending token sale)
-		admin = msg.sender; // address of person who deployed the contract
+		
+		admin = msg.sender;	// assign an admin (an external account connected to the blockchain that has special privleges that other accounts do not have, such as ending token sale)
+		 					// address of person who deployed the contract
+		tokenContract = _tokenContract;	// add token contract, because all token functionality can be found there
+		 								// gets passed value in deploy_contracts.js
+		tokenPrice = _tokenPrice; // assign token price	
+	}
 
-		// add token contract, because all token functionality can be found there
-		tokenContract = _tokenContract; // gets passed value in deploy_contracts.js
+	// multiply function - safe arithmetic
+	function multiply(uint x, uint y) internal pure returns (uint z) { // internal means that it is used only internally within contract, cannot be called externally
+																	   // pure means that it is not acutally reading/writing in blockchain; does not create a transaction
+		require(y == 0 || (z = x * y) / y == x);
+	}
 
-		// assign token price
-		tokenPrice = _tokenPrice;
+	// buy tokens
+	function buyTokens(uint256 _numberOfTokens) public payable { // made public so visible to public interface, allowing user interaction. 
+																// function made payable so it can process transactions.
+		
+		require(msg.value == multiply(_numberOfTokens, tokenPrice)); // require that value is equal to tokens (help prevent people from overpaying or underpaying for thier token amount)
+		 													// msg is a global variable with different properties such as sender and value
+															// msg.value is amount of wei function is sending
+		
+		require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);	// require that contract has enough tokens (take some tokens from token supply and give it to this contract)
+
+		require(tokenContract.transfer(msg.sender, _numberOfTokens));	// require that a transfer is successful
+																		// our buy functionality
+		
+		tokensSold += _numberOfTokens; 	// keep track of tokens sold
+		 								// whenever someone buys tokens increment number of tokens sold
+
+		emit sell(msg.sender, _numberOfTokens); // trigger a sell event
+										// buyer is the account that is calling the function (msg.sender)
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,14 +1,14 @@
 //In charge of migrating smart contracts
 
-var MyToken = artifacts.require("./MyToken.sol"); //Create an instance of MyToken
-											  //truffle-artifacts library which is itself a wrapper around the truffle-contract library which in turns wraps around the web3.js library.
-											  //Creating an artifact allows us to create a contract abstraction that Truffle can use to run in a js runtime environment. This has several different applications, it allows us to interact with our smart contract in any js environment like Truffle or when writing tests. 
+var MyToken = artifacts.require("./MyToken.sol"); // Create an instance of MyToken
+											  // truffle-artifacts library which is itself a wrapper around the truffle-contract library which in turns wraps around the web3.js library.
+											  // Creating an artifact allows us to create a contract abstraction that Truffle can use to run in a js runtime environment. This has several different applications, it allows us to interact with our smart contract in any js environment like Truffle or when writing tests. 
 
 var MyTokenSale = artifacts.require("./MyTokenSale.sol");
 
 module.exports = function (deployer) {
-  deployer.deploy(MyToken, 1000000).then(function() {//Deploy MyToken, subsequent arguments are passed to constructor in MyToken.sol, such as supply
-  							//returns an instance of the deployed promise MyToken object
+  deployer.deploy(MyToken, 1000000).then(function() { // deploy MyToken, subsequent arguments are passed to constructor in MyToken.sol, such as supply
+  							// returns an instance of the deployed promise MyToken object
 				// token price is 0.001 ether
 				var tokenPrice = 1000000000000000;
 				return  deployer.deploy(MyTokenSale, MyToken.address, tokenPrice); // pass address of token contract
