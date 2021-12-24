be0648c4eadee9fa1f0f5b1f654eda5fe9f87e0e
Tahmid Zaman <tahmid198@yahoo.com>
2021-12-24 00:25:27 -0500

    render all values
    render values such as token price, token available, token sold, user account address

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,10 @@ App = {
 		web3Provider: null,
 		contracts: {},
 		account: '0x0',
+		loading: false,
+		tokenPrice: 1000000000000000, // default token price in wei
+		tokensSold: 0,
+		tokensAvailable: 750000,
 
 		init:function() {
 			console.log("App initialized...")
@@ -44,19 +48,65 @@ App = {
 			});
 		})
 	},
+
 	render: function() { 
+		// if app is loading dont show content
+		if(App.loading) { // if true we will skip step.
+			return; 
+		}
+		// whenever loading is finished we continue
+		App.loading = true;
+
+		// keep track of loader and content from index.html
+		var loader = $('#loader');
+		var content = $('#content');
+
+		loader.show();
+		content.hide();
+
 		// load account data
 		// we need to queary for account address and wire it to the frontend index.html
 		web3.eth.getCoinbase(function(err, account) {
 			if(err == null) {
-				console.log("account", account);
+				// console.log("account", account);
 				App.account = account; 
 				$('#accountAddress').html("Your Account: " + account)
 			}
 		})
 
+		// token price will be read from smart contract
+		App.contracts.MyTokenSale.deployed().then(function(instance) {
+			myTokenSaleInstance = instance;
+			return myTokenSaleInstance.tokenPrice();
+		}).then(function(tokenPrice) {
+			// console.log("tokenPrice", tokenPrice)
+			App.tokenPrice = tokenPrice;
+			$('.token-price').html(web3.fromWei(App.tokenPrice, "ether").toNumber()); // convert wei to ether
+			return myTokenSaleInstance.tokensSold();
+		}).then(function(tokenSold) {
+			App.tokensSold = tokenSold.toNumber();
+			$('.tokens-sold').html(App.tokensSold);
+			$('.tokens-available').html(App.tokensAvailable);
+
+			var progressPercent = (Math.ceil(App.tokensSold / App.tokensAvailable)) * 100;
+			// console.log(progressPercent);
+			$('#progress').css('width', progressPercent + '%');
+
+			// load token contract and balance
+			App.contracts.MyToken.deployed().then(function(instance) {
+				myTokenInstance = instance;
+				return myTokenInstance.balanceOf(App.account);
+				}).then(function(balance) {
+					$('.mytoken-balance').html(balance.toNumber())
+				
+					App.loading = false; // once loaded show content
+					loader.hide();
+					content.show();
+				})
+			});
+		}
 	}
-}
+
 
 
 // whenever window loads initilize our app
